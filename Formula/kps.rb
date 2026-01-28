40d58d23d636a9e44ff7d2f4923e837bf6cee224
zaehorang <jaho001@uos.ac.kr>
2026-01-28 23:05:33 +0900

    chore: reset Formula to placeholder for next release
    Universal binary structure is now established.
    Next release will automatically update this via CI/CD.

--- Formula/kps.rb
+++ Formula/kps.rb
@@ -1,11 +1,11 @@
 class Kps < Formula
   desc "Algorithm problem-solving tracker for BOJ & Programmers"
   homepage "https://github.com/zaehorang/KPSTool"
-  version "0.2.0-test"
+  version "0.1.1"
   license "MIT"
 
-  url "https://github.com/zaehorang/KPSTool/releases/download/v0.2.0-test/kps-v0.2.0-test.tar.gz"
-  sha256 "5c48a8a7b50a73518d2e22ef158bbf46330121133c3d12052e7a55d81f91bf3c"
+  url "https://github.com/zaehorang/KPSTool/releases/download/v0.1.1/kps-v0.1.1.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"
 
   def install
     bin.install "kps"
