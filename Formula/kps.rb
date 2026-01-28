0c149e577891bf1d9eb55f3743959678f890efcb
zaehorang <zaehorang@users.noreply.github.com>
2026-01-28 13:58:09 +0000

    chore: update kps to v0.2.0-test

--- Formula/kps.rb
+++ Formula/kps.rb
@@ -1,16 +1,16 @@
 class Kps < Formula
   desc "Algorithm problem-solving tracker for BOJ & Programmers"
   homepage "https://github.com/zaehorang/KPSTool"
-  version "0.1.1"
+  version "0.2.0-test"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/zaehorang/KPSTool/releases/download/v0.1.1/kps-arm64-v0.1.1.tar.gz"
-      sha256 "b2b002b7f1862dea0a69d4fe404a5dece1dad70072774f57c8d25170c71c0b34"
+      url "https://github.com/zaehorang/KPSTool/releases/download/v0.2.0-test/kps-v0.2.0-test.tar.gz"
+      sha256 "5c48a8a7b50a73518d2e22ef158bbf46330121133c3d12052e7a55d81f91bf3c"
     elsif Hardware::CPU.intel?
-      url "https://github.com/zaehorang/KPSTool/releases/download/v0.1.1/kps-x86_64-v0.1.1.tar.gz"
-      sha256 "1aa401a42950e62401deb9204e161e1bef93ad4e3f02201a2a642e900be689dd"
+      url "https://github.com/zaehorang/KPSTool/releases/download/v0.2.0-test/kps-v0.2.0-test.tar.gz"
+      sha256 "5c48a8a7b50a73518d2e22ef158bbf46330121133c3d12052e7a55d81f91bf3c"
     end
   end
 
