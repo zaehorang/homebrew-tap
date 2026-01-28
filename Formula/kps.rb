3e223726621b1c0b871db536c8141b0cb1dfee34
zaehorang <zaehorang@users.noreply.github.com>
2026-01-28 14:10:37 +0000

    chore: update kps to v0.2.0

--- Formula/kps.rb
+++ Formula/kps.rb
@@ -1,11 +1,11 @@
 class Kps < Formula
   desc "Algorithm problem-solving tracker for BOJ & Programmers"
   homepage "https://github.com/zaehorang/KPSTool"
-  version "0.1.1"
+  version "0.2.0"
   license "MIT"
 
-  url "https://github.com/zaehorang/KPSTool/releases/download/v0.1.1/kps-v0.1.1.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  url "https://github.com/zaehorang/KPSTool/releases/download/v0.2.0/kps-v0.2.0.tar.gz"
+  sha256 "6679a23199cc1bfd3fda32b8df55d9c9ed3dc15e8b514109d62135da4969e807"
 
   def install
     bin.install "kps"
