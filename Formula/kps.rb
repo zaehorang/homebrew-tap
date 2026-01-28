b16de01c980842a4901c9163fb43f10af204a06f
zaehorang <jaho001@uos.ac.kr>
2026-01-28 23:02:35 +0900

    fix: convert Formula to Universal binary structure
    - Remove on_macos arch-specific blocks
    - Use single URL/SHA256 for Universal binary
    - Simplify install method (direct kps binary)
    
    This completes the migration to Universal binary release.
    Future releases will be automatically updated by CI/CD with simple sed commands.

--- Formula/kps.rb
+++ Formula/kps.rb
@@ -4,18 +4,11 @@ class Kps < Formula
   version "0.2.0-test"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/zaehorang/KPSTool/releases/download/v0.2.0-test/kps-v0.2.0-test.tar.gz"
-      sha256 "5c48a8a7b50a73518d2e22ef158bbf46330121133c3d12052e7a55d81f91bf3c"
-    elsif Hardware::CPU.intel?
-      url "https://github.com/zaehorang/KPSTool/releases/download/v0.2.0-test/kps-v0.2.0-test.tar.gz"
-      sha256 "5c48a8a7b50a73518d2e22ef158bbf46330121133c3d12052e7a55d81f91bf3c"
-    end
-  end
+  url "https://github.com/zaehorang/KPSTool/releases/download/v0.2.0-test/kps-v0.2.0-test.tar.gz"
+  sha256 "5c48a8a7b50a73518d2e22ef158bbf46330121133c3d12052e7a55d81f91bf3c"
 
   def install
-    bin.install Hardware::CPU.arm? ? "kps-arm64" : "kps-x86_64" => "kps"
+    bin.install "kps"
   end
 
   test do
